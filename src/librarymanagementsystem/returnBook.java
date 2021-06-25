861ad29c8399eb431a5350ef6ac2ddc719ee7b43
Hassan Ajmal <hassanajmal531@gmail.com>
2021-06-26 00:52:51 +0500

    Allah Ka Shukr kaam Khtm

--- src/librarymanagementsystem/returnBook.java
+++ src/librarymanagementsystem/returnBook.java
@@ -73,7 +73,7 @@ public class returnBook extends javax.swing.JFrame {
             }
         });
 
-        jButton2.setText("jButton2");
+        jButton2.setText("close");
         jButton2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton2ActionPerformed(evt);
